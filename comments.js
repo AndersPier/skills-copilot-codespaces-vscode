10a0460c267cde0a8d98f8fe80503ce03f4ebc18
Anders Lehmann Pier <anders@pier.dk>
2025-01-14 01:19:39 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,33 @@
+// http://localhost:3000/comments
+// http://localhost:3000/comments/:id
+// http://localhost:3000/comments?postId=1
+
+const express = require('express');
+const router = express.Router();
+
+const db = require('../db');
+
+router.get('/', (req, res) => {
+  const postId = req.query.postId;
+  const query = postId ? { postId } : {};
+
+  db.find('comments', query)
+    .then(comments => res.json(comments))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.findOne('comments', { id })
+    .then(comment => {
+      if (comment) {
+        res.json(comment);
+      } else {
+        res.status(404).json({ error: 'Comment not found' });
+      }
+    })
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+module.exports = router;
